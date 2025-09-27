fcc2cb2bbb9e0a85c4a0196aa22dfc0a8f29472f
Charukhesh <charukheshbr33@gmail.com>
2025-09-28 00:50:13 +0530

    Cool version

--- main.m
+++ main.m
@@ -1,127 +1,159 @@
 clear; close all;
 
-Nx = 40;   % No.of cells in x-direction
-Ny = 40;   % No.of cells in y-direction
 Lx = 1;    % Length of the mesh in x-direction
 Ly = 0.5;  % Length of the mesh in y-direction
-%% Plotting the mesh
-str_x = 25; str_y = 15;
-[x_lineee, y_lineee, dx, dy, nodesx, nodesy] = generateMesh(Nx, Ny, Lx, Ly, str_x, str_y);
 
-figure(1);
-xline(x_lineee);
-yline(y_lineee);
-hold on;
-for c = 1:length(nodesx)
-    plot(nodesx(c), nodesy,'r.','MarkerSize',10);
+fprintf(['Plot the mesh: Section no.1 \nGauss Seidel Solver: Section no.2 \n' ...
+    'Prove mesh independence: Section no.3 \nPlot Heat flow (vector plot): Section no.4 \n'])
+section = input('What do you want to do? Pls enter the Section No. ');
+
+if section ~= 3
+    Nx = input('Enter the no.of cells you want in x-direction: ');
+    Ny = input('Enter the no.of cells you want in y-direction: ');
+    str_x = input('Enter the stretch(%) you would like in x-direction: ');
+    str_y = input('Enter the stretch(%) you would like in y-direction:  ');
 end
-hold on;
-for r = 1:length(nodesx)
-    plot(nodesx, nodesy(r),'r.','MarkerSize',10);
-end
-hold off;
-%%
-epsilon = 1e-4; % Error Tolerance for convergence
-
-T = zeros(Ny+2, Nx+2);
-num_iter = 100000;
-for n = 1:num_iter
-    fprintf('Iteration no. %d \n', n)
-    [T, cnvrg_val] = GaussSeidelSolver(T, nodesx, nodesy, dx, dy, Nx, Ny, Lx, Ly);
-    residuals(n) = cnvrg_val;
-    disp(cnvrg_val)
-
-    if cnvrg_val < epsilon
-        disp('Convergence has been achieved')
-        break
-    else
-        disp('Not yet, try harder')
+%% Plotting the mesh (Section-1)
+if section == 1
+    [x_lineee, y_lineee, dx, dy, nodesx, nodesy] = generateMesh(Nx, Ny, Lx, Ly, str_x, str_y);
+    
+    figure(1);
+    xline(x_lineee);
+    yline(y_lineee);
+    hold on;
+    for c = 1:length(nodesx)
+        plot(nodesx(c), nodesy,'r.','MarkerSize',10);
     end
+    hold on;
+    for r = 1:length(nodesx)
+        plot(nodesx, nodesy(r),'r.','MarkerSize',10);
+    end
+    hold off;
 end
-%% Checking mesh independence
-% Nx = [10, 20, 40, 80];
-% Ny = [10, 20, 40, 80];
-% str_x = 20; str_y = -20;
-%
-% [x_lineee, y_lineee, dx, dy, nodesx, nodesy] = generateMesh(Nx(l), Ny(l), Lx, Ly, str_x, str_y);
-% epsilon = 1e-4; % Error Tolerance for convergence
-% num_iter = 100000;
-% for l = 1:length(Nx)
-%     fprintf('Solving for %dx%d \n', Nx(l), Ny(l))
-%     T = zeros(Ny(l)+2, Nx(l)+2);
-%
-%     for n = 1:num_iter
-%         [T, cnvrg_val] = GaussScidelSolver(T, nodesx, nodesy, dx, dy, Nx(l), Ny(l), Lx, Ly);
-%         if cnvrg_val < epsilon
-%             break
-%         else
-%             continue
-%         end
-%     end
-%
-%     [X, Y] = meshgrid(nodesx, nodesy);
-%
-%     avgT_LeftSec(l)   = mean(T(X <= Lx/3));
-%     avgT_MiddleSec(l) = mean(T(X > Lx/3 & X <= 2*Lx/3));
-%     avgT_RightSec(l)  = mean(T(X > 2*Lx/3));
-%     disp('Done')
-% end
-%
-% fprintf('Std Deviation of Avg Temp in left section is %.2f \n', std(avgT_LeftSec))
-% fprintf('Std Deviation of Avg Temp in middle section is %.2f \n', std(avgT_MiddleSec))
-% fprintf('Std Deviation of Avg Temp in right section is %.2f \n', std(avgT_RightSec))
-%%
-dTdx = zeros(Ny+2, Nx+2);
-dTdy = zeros(Ny+2, Nx+2);
-
-[X_nodes, Y_nodes] = meshgrid(nodesx, nodesy);
-for i = 1:Ny+2
-    dTdx(i, 1) = (T(i, 2) - T(i, 1)) / (nodesx(2) - nodesx(1));
-
-    for j = 2:Ny+1
-        dTdx(i, j) = (T(i, j+1) - T(i, j-1)) / (nodesx(j+1) - nodesx(j-1));
+%% Solving using Gauss-Seidel Method (Section-2) 
+if section == 2
+    
+    [x_lineee, y_lineee, dx, dy, nodesx, nodesy] = generateMesh(Nx, Ny, Lx, Ly, str_x, str_y);
+    epsilon = 1e-4; % Error Tolerance for convergence
+    
+    T = zeros(Ny+2, Nx+2);
+    num_iter = 100000;
+    for n = 1:num_iter
+        fprintf('Iteration no. %d \n', n)
+        [T, cnvrg_val] = GaussSeidelSolver(T, nodesx, nodesy, dx, dy, Nx, Ny, Lx, Ly);
+        residuals(n) = cnvrg_val;
+        disp(cnvrg_val)
+    
+        if cnvrg_val < epsilon
+            disp('Convergence has been achieved')
+            break
+        else
+            disp('Not yet, try harder')
+        end
     end
 
-    dTdx(i, end) = (T(i, end) - T(i, end-1)) / (nodesx(end) - nodesx(end-1));
+    % Convergence Plot
+    figure;
+    plot(1:length(residuals), residuals, '-o');
+    title('Convergence History');
+    xlabel('Iteration Number');
+    ylabel('Residual');
+    grid on;
 end
-
-
-for j = 1:Nx+2
-    dTdy(1, j) = (T(2, j) - T(1, j)) / (nodesy(2) - nodesy(1));
-
-    for i = 2:Ny+1
-        dTdy(i, j) = (T(i+1, j) - T(i-1, j)) / (nodesy(i+1) - nodesy(i-1));
+%% Checking mesh independence (Section-3)
+if section == 3
+    Nx = [10, 20, 40, 80];
+    Ny = [10, 20, 40, 80];
+    str_x = 0; str_y = 0;
+    
+    [x_lineee, y_lineee, dx, dy, nodesx, nodesy] = generateMesh(Nx(l), Ny(l), Lx, Ly, str_x, str_y);
+    epsilon = 1e-4; % Error Tolerance for convergence
+    num_iter = 100000;
+    for l = 1:length(Nx)
+        fprintf('Solving for %dx%d \n', Nx(l), Ny(l))
+        T = zeros(Ny(l)+2, Nx(l)+2);
+    
+        for n = 1:num_iter
+            [T, cnvrg_val] = GaussSeidelSolver(T, nodesx, nodesy, dx, dy, Nx(l), Ny(l), Lx, Ly);
+            if cnvrg_val < epsilon
+                break
+            else
+                continue
+            end
+        end
+    
+        [X, Y] = meshgrid(nodesx, nodesy);
+    
+        avgT_LeftSec(l)   = mean(T(X <= Lx/3));
+        avgT_MiddleSec(l) = mean(T(X > Lx/3 & X <= 2*Lx/3));
+        avgT_RightSec(l)  = mean(T(X > 2*Lx/3));
+        disp('Done')
+    end
+    
+    fprintf('Std Deviation of Avg Temp in left section is %.2f \n', std(avgT_LeftSec))
+    fprintf('Std Deviation of Avg Temp in middle section is %.2f \n', std(avgT_MiddleSec))
+    fprintf('Std Deviation of Avg Temp in right section is %.2f \n', std(avgT_RightSec))
+end
+%% Plotting Heat Flow (Section-4)
+if section == 4
+    str_x = 25; str_y = 15;
+    [x_lineee, y_lineee, dx, dy, nodesx, nodesy] = generateMesh(Nx, Ny, Lx, Ly, str_x, str_y);
+    epsilon = 1e-4; % Error Tolerance for convergence
+    
+    T = zeros(Ny+2, Nx+2);
+    num_iter = 100000;
+    for n = 1:num_iter
+        [T, cnvrg_val] = GaussSeidelSolver(T, nodesx, nodesy, dx, dy, Nx, Ny, Lx, Ly);   
+        if cnvrg_val < epsilon
+            break
+        else
+            continue
+        end
     end
 
-    dTdy(end, j) = (T(end, j) - T(end-1, j)) / (nodesy(end) - nodesy(end-1));
+    dTdx = zeros(Ny+2, Nx+2);
+    dTdy = zeros(Ny+2, Nx+2);
+    
+    [X_nodes, Y_nodes] = meshgrid(nodesx, nodesy);
+    for i = 1:Ny+2
+        dTdx(i, 1) = (T(i, 2) - T(i, 1)) / (nodesx(2) - nodesx(1));
+    
+        for j = 2:Ny+1
+            dTdx(i, j) = (T(i, j+1) - T(i, j-1)) / (nodesx(j+1) - nodesx(j-1));
+        end
+    
+        dTdx(i, end) = (T(i, end) - T(i, end-1)) / (nodesx(end) - nodesx(end-1));
+    end
+    
+    for j = 1:Nx+2
+        dTdy(1, j) = (T(2, j) - T(1, j)) / (nodesy(2) - nodesy(1));
+    
+        for i = 2:Ny+1
+            dTdy(i, j) = (T(i+1, j) - T(i-1, j)) / (nodesy(i+1) - nodesy(i-1));
+        end
+    
+        dTdy(end, j) = (T(end, j) - T(end-1, j)) / (nodesy(end) - nodesy(end-1));
+    end
+    
+    K = 16 .* (nodesy / Ly) + 16;
+    qx = -K .* dTdx;
+    qy = -K .* dTdy;
+    
+    % Heat-flux Vector plot
+    figure;
+    contourf(X_nodes, Y_nodes, T, 20, "LineStyle", 'none');
+    c = colorbar;
+    c.Label.String = 'Temperature';
+    hold on;
+    
+    quiver(X_nodes, Y_nodes, qx, qy, 'k', 'AutoScaleFactor', 1.5);
+    
+    hold off;
+    title('Vector plot of Heat Flux');
+    xlabel('x');
+    ylabel('y');
+    set(gca, 'YDir', 'normal');
 end
-
-K = 16 .* (nodesy / Ly) + 16;
-qx = -K .* dTdx;
-qy = -K .* dTdy;
-%% Plots
-% Convergence Plot
-figure;
-plot(1:length(residuals), residuals, '-o');
-title('Convergence History');
-xlabel('Iteration Number');
-ylabel('Residual');
-grid on;
-
-% Heat-flux Vector plot
-figure;
-contourf(X_nodes, Y_nodes, T, 20, "LineStyle", 'none');
-c = colorbar;
-c.Label.String = 'Temperature';
-hold on;
-
-quiver(X_nodes, Y_nodes, qx, qy, 'k', 'AutoScaleFactor', 1.5);
-
-hold off;
-title('Vector plot of Heat Flux');
-xlabel('x');
-ylabel('y');
-set(gca, 'YDir', 'normal');
 %% Gauss Scidel Iterative Solver (Case-4)
 function [T, cnvrg_val] = GaussSeidelSolver(T, nodesx, nodesy, dx, dy, Nx, Ny, Lx, Ly)
 S = -1.5; q = -5000;
